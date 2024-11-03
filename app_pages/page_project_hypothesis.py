143cce67dcc9ae96dd185b949749edd9d56da5c4
Callum Priestman <callum.priestman@hotmail.co.uk>
2024-11-03 11:12:20 +0000

    Projecy hypothesis 1 added to hypothesis page

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -3,10 +3,28 @@ import matplotlib.pyplot as plt
 
 
 def page_project_hypothesis_body():
-    st.write("### Project Hypothesis and Validation")
+    st.write("## Project Hypothesis and Validation")
+
+    st.write("---")
+
+    st.write("### Hypothesis 1 and Validation:")
 
     st.success(
-        f"* We suspect that trees with powdery mildew have clear white markers on the leaves,\n"
-        f" this can differentiate them from a healthy tree."
-        
+        f"* Trees with powdery mildew have clear white markers on the leaves,\n"
+        f" this can differentiate them from a healthy tree."    
     )
+
+    st.info(
+        f"We suspect that cherry leaves that are affected by powdery mildew show"
+        f" clear markers on the surface of the leaf. Usually a white cotton-like growth appears to form around the leaf."
+    )
+
+    st.write("(To see this investigated thoroughly visit the mildew visualizer page.)")
+
+    st.warning(
+        f"The model was successful in determining the difference between healthy and affected leaves, and is now able to predict"
+        f" with high levels of accuracy on unseen data. The model was trained to predict based on unseen data rather than adhere"
+        f" too closely to the training data. This meant that it did not 'memorize' the training data allowing the business to use"
+        f" the model in future cases with confidence in its accuracy."
+    )
+
